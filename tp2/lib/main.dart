ca73f066ebf8c9b22ffe1ea0909b5243044a2d9e
Bantortek <148080660+Bantortek@users.noreply.github.com>
2025-02-24 18:03:08 +0100

    Ex2

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(const MyApp());
@@ -29,15 +32,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double speed = 0.01;
+  double sliderValueX = 0;
+  double sliderValueZ = 0;
+  double sliderValueScale = 0.5;
+  bool isMirrored = false;
+  bool isAuto = false;
+  Image img = Image.network("https://picsum.photos/512/1024", height: 500,);
+  Image img_mod = Image.network("https://picsum.photos/512/1024", height: 500,);
 
+  void animate(Timer t) {
+    setState(() {
+      sliderValueScale += speed;
+      sliderValueX += speed;
+      sliderValueZ += speed;
+      if (sliderValueScale>1){
+        sliderValueScale -=1;
+      }
+      if (sliderValueX>1){
+        sliderValueX -=1;
+      }
+      if (sliderValueZ>1){
+        sliderValueZ -=1;
+      }
+      t.cancel();
+    });
+      
+  }
   @override
   Widget build(BuildContext context) {
+    if(isAuto){
+      const d = Duration(milliseconds: 50);
+      Timer.periodic(d, animate);
+    }
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      appBar: AppBar(title: const Text('Rotate/Resize image')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.skewY(0)..rotateX(math.pi*2*sliderValueX),
+                      child:Transform.scale(scale: 2*sliderValueScale,
+                        child : Transform.rotate(angle: math.pi*2*sliderValueZ, 
+                          child: Transform.flip(flipX: isMirrored, child: img_mod)
+                          )
+                        )
+              ) 
+          ),
+          Slider(
+            value: sliderValueX,
+            onChanged: (double value) {
+              setState(() {
+                sliderValueX = value;
+              });
+            },
+          ),
+          Slider(
+            value: sliderValueZ,
+            onChanged: (double value) {
+              setState(() {
+                sliderValueZ = value;
+              });
+            },
+          ),
+          Checkbox(
+            checkColor: Colors.white,
+            value: isMirrored,
+            onChanged: (bool? value) {
+              setState(() {
+                isMirrored = value!;
+              });
+            },
+          ),
+          Checkbox(
+            checkColor: Colors.white,
+            value: isAuto,
+            onChanged: (bool? value) {
+              setState(() {
+                isAuto = value!;
+              });
+            },
+          ),
+          Slider(
+            value: sliderValueScale,
+            onChanged: (double value) {
+              setState(() {
+                sliderValueScale = value;
+                print(value);
+              });
+            },
+          ),
+        ],
       ),
-      body: Image.network("https://picsum.photos/512/1024"),
     );
   }
 }
